b4add7e53807a69f117681af6b95b5d4f9fee9b3
Chikrix <itschidi5s@gmail.com>
2017-11-08 18:55:12 +0100

    Clean up

--- app/src/main/java/com/toknfc/nfctok/presenters/HomeActivityPresenter.kt
+++ app/src/main/java/com/toknfc/nfctok/presenters/HomeActivityPresenter.kt
@@ -5,31 +5,15 @@ import com.toknfc.nfctok.core.Presenter
 
 /**
  * Created by Chidi Justice
- *
- * Presenters should be free of android sdk classes
- */
+ **/
 class HomeActivityPresenter(private val view: View) : Presenter {
 
   fun init() {
     view.showHomeScreen()
   }
 
-  /**
-   * From the app, it is designed to handle tags written to from the app, or Andela Tags.
-   * Andela tags has a single record written to it, in my db, it'll be my ID.
-   *
-   * Some edge cases, In a case where the same record is inserted twice
-   */
   fun handlePayloadFromNfcTag(payload: String, hasId: ByteArray?) {
-    //1. navigate to tagInfo screen with the text.
     view.openTagInfoScreen(payload)
-    /**
-     * Ensure that andela tags isn't saved multiple times. Each tag should have a unique ID,
-     * which should be used to distinguish them from each other. For Andela tags, first check
-     * that no id with its payload exists, before saving. For new ones, save an id to the tag,
-     * which would be used for distinquishing them in the db
-     */
-    //2. Save payload on the background
   }
 
   override fun dispose() {
--- app/src/main/java/com/toknfc/nfctok/views/activities/HomeActivity.kt
+++ app/src/main/java/com/toknfc/nfctok/views/activities/HomeActivity.kt
@@ -6,12 +6,12 @@ import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.os.Parcelable
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import com.toknfc.nfctok.R
 import com.toknfc.nfctok.R.layout
 import com.toknfc.nfctok.R.string
 import com.toknfc.nfctok.core.BUNDLE_TAG_PAYLOAD
 import com.toknfc.nfctok.core.BasicFragmentManager
-import com.toknfc.nfctok.core.CoreActivity
 import com.toknfc.nfctok.core.NFC_BUNDLE_KEY
 import com.toknfc.nfctok.presenters.HomeActivityPresenter
 import com.toknfc.nfctok.views.fragments.HomeFragment
@@ -20,7 +20,7 @@ import com.toknfc.nfctok.views.fragments.WriteToNfcTagFragment
 import kotlinx.android.synthetic.main.activity_home.root
 
 
-class HomeActivity : CoreActivity(), HomeActivityPresenter.View {
+class HomeActivity : AppCompatActivity(), HomeActivityPresenter.View {
 
   private val presenter: HomeActivityPresenter by lazy {
     HomeActivityPresenter(this)
